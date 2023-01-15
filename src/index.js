af51598bc370bc2d22e9de455736884924c97979
Lucas Jandrew <fapiko@gmail.com>
2023-01-14 21:21:53 -0500

    Add google analytics

--- src/index.js
+++ src/index.js
@@ -3,15 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactGA from 'react-ga4';
+
+const TRACKING_ID = 'G-ZF06EHENBW';
+
+ReactGA.initialize(TRACKING_ID);
+ReactGA.send('pageview');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function sendToAnalytics({id, name, value, label}) {
+    ReactGA.event(
+        {
+            eventCategory: 'Web Vitals',
+            eventAction:   name,
+            eventValue:    Math.round(name === 'CLS' ? value * 1000 : value), // values must be
+                                                                              // integers
+            eventLabel:     id, // id unique to current page load
+            nonInteraction: true, // avoids affecting bounce rate
+        });
+}
+
+reportWebVitals(sendToAnalytics);
